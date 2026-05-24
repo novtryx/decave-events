9fb89eff824a9cec7cf090fb0c2ea59f643c5f7d
emmanuelkawekwune <emmanuelkawekwune@punch.agency>
2026-05-24 09:02:14 +0100

    influencer

--- app/influencers/page.tsx
+++ app/influencers/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import InfluencerPage from './Home'
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-const page = () => {
+const page = async() => {
+  const ACCESS_TOKEN_KEY_2 = "accessToken2"
+
+    const token = (await cookies()).get(ACCESS_TOKEN_KEY_2)?.value;
+
+  if (!token) {
+    redirect("/influencers/auth/login");
+  }
   return (
     <InfluencerPage/>
   )
